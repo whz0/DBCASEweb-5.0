04b0c61ef50b9c7a77c651cd03e656813b1840ad
whz0 <whongz86@gmail.com>
2026-01-31 13:28:12 +0100

    Arquitectura inicial Vuejs Spring

---
+++ DBCASE-5.0/backend/src/main/java/com.tfg.ucm/Application.java
@@ -0,0 +1,18 @@
+package com.tfg.ucm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Application's main method
+     *
+     * @param args the console arguments
+     */
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
